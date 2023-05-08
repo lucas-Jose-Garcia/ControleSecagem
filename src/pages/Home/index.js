92b3c85ed67e82a4ffa1bd4ce745016d26102330
lucas-Jose-Garcia <lucasjosegarcia01@gmail.com>
2023-05-07 21:09:23 -0300

    Nova tentativa

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { styles } from '../../style/styles';
--- src/router/drawer.routes.js
+++ src/router/drawer.routes.js
@@ -1,3 +1,4 @@
+import * as React from 'react';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Home from '../pages/Home';
 import AddReleases from '../pages/AddReleases';
